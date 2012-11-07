b3657d303d2271bcd50b277f57d55a2f4729df38
Thomas Levine <not-an-email-address@thomaslevine.com>
2012-11-07 03:25:17 +0000

    dependency installation

---
+++ worker/dependencies-user.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+if [ "$USER" = 'root' ]
+  then
+  echo Don\'t run this as root.
+  exit 1
+else
+  if python2 --version | grep 2.7.3; then
+    echo 'Using the installed python 2.7.3'
+  else
+    echo 'Installing python 2.7.3'
+
+    # pythonz
+    curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+    source "$HOME/.pythonz/etc/bashrc"
+    echo '[[ -s "$HOME/.pythonz/etc/bashrc" ]] && source "$HOME/.pythonz/etc/bashrc"' >> "$HOME/.bashrc"
+
+    # python
+    pythonz install 2.7.3
+
+  fi
+
+  # virtualenv
+  virtualenv -p python2.7 "$HOME/env"
+  source "$HOME/env/bin/activate"
+
+# python3 --version| grep 3.3.0 || pythonz install 3.3.0
+# virtualenv -p "$HOME/.pythonz/pythons/CPython-3.3.0/bin/python3.3" python3.3.0
+
+  # packages
+  pip install mincemeat
+
+fi
--- worker/run.sh
+++ worker/run.sh
@@ -5,7 +5,7 @@ password='ae145b3c-2880-11e2-b278-f23c91ae1986'
 server_address='box.scraperwiki.com'
 
 # Run
-python mincemeat.py -p "$password" "$server_address"
+python -m mincemeat -p "$password" "$server_address" 
 
 # Report
 echo The worker is running. It is watching the server at \""$server_address"\"
