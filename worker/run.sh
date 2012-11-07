ef2478413994a82ddb6277057497b714f182d8f2
Thomas Levine <not-an-email-address@thomaslevine.com>
2012-11-07 04:05:21 +0000

    run multiple workers

--- worker/run.sh
+++ worker/run.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -e
 
 # Parameters
 password='ae145b3c-2880-11e2-b278-f23c91ae1986'
 server_address='box.scraperwiki.com'
 
 # Run
-python -m mincemeat -p "$password" "$server_address" 
+nprocs=$(grep -c processor /proc/cpuinfo)
+echo Running $nprocs worker processes
+for process in $(seq $nprocs); do
+  python -m mincemeat -p "$password" "$server_address" &
+done
+wait
+echo The job is complete.
